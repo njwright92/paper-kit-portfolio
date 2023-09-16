9ca095e984e00dc5dcde7e57856a4e9f2bd4c511
njwright92 <njwright92@gmail.com>
2023-09-16 14:52:28 -0700

    "updates to skills icons"

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -40,24 +40,26 @@ function IndexHeader() {
                 Nathan John Wright
               </em>
             </h3>
-            <div className="circle mx-auto">
-              <i className="fab fa-lg fa-js logo logo-js"></i>
-              <i className="fab fa-lg fa-react logo logo-react"></i>
-              <i className="fas fa-lg fa-mobile-alt logo logo-react-native"></i>
-              <i className="fab fa-lg fa-google logo logo-gcp"></i>
-              <i className="fab fa-lg fa-bootstrap logo logo-bootstrap"></i>
-              <i className="fab fa-lg fa-node-js logo logo-nodejs"></i>
-            </div>
-            <div className="title-brand">
-              <h1 className="presentation-title">Code Showcase</h1>
-              <div className="fog-low">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
+            <div className="container">
+              <div className="circle">
+                <i className="fab fa-lg fa-js logo logo-js"></i>
+                <i className="fab fa-lg fa-react logo logo-react"></i>
+                <i className="fas fa-lg fa-mobile-alt logo logo-react-native"></i>
+                <i className="fab fa-lg fa-google logo logo-gcp"></i>
+                <i className="fab fa-lg fa-bootstrap logo logo-bootstrap"></i>
+                <i className="fab fa-lg fa-node-js logo logo-nodejs"></i>
               </div>
+              <div className="title-brand">
+                <h1 className="presentation-title">Code Showcase</h1>
+                <div className="fog-low">
+                  <img alt="..." src={require("assets/img/fog-low.png")} />
+                </div>
+                <div className="fog-low right">
+                  <img alt="..." src={require("assets/img/fog-low.png")} />
+                </div>
+              </div>
+            </div>
 
-              <div className="fog-low right">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
-              </div>
-            </div>
             <h2 className="presentation-subtitle text-center">
               <em>
                 "Your best investment is in yourself"
