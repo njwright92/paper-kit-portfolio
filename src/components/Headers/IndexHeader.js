417d60a708e12f745ab598faf82176e9db5d6a49
njwright92 <njwright92@gmail.com>
2023-10-23 12:31:59 -0700

    "new header photo- Fernan lake"

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -29,7 +29,7 @@ function IndexHeader() {
         className="page-header section-dark"
         style={{
           backgroundImage:
-            "url(" + require("assets/img/daniel-olahs.jpg") + ")",
+            "url(" + require("assets/img/fernan.JPG") + ")",
         }}
       >
         <div className="filter" />
